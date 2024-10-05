81b95f41580d089f7535b7832d6bdf048d590616
Pablo Quiceno <pabloquiceno95@gmail.com>
2024-10-05 10:13:56 -0500

    changes with layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { SelectCurrencies } from './components/SelectCurrencies'
+import Container from '@mui/material/Container';
 
 const App = () => {
 
   return (
-    <>
+    <Container>
       <h1>Convertidor de monedas</h1>
 
       <SelectCurrencies />
-    </>
+    </Container>
   )
 }
 
